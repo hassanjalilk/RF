0a0e5ce681f9529c1406f151826ecd4de2360a3c
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-23 23:06:56 -0400

    tried to fix review stuff

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,37 +1,37 @@
-class ReviewController < ApplicationController
+
+
+class ReviewsController < ApplicationController
+  before_filter :load_product
 
   def show
-  	@review = Review.find(params[:id])
+    @review = Review.find(params[:id])
   end
 
   def create
-  	@review = @product.reviews.build(review_params)
-  	@review.user_id = cureent_user.id
+    @review = @product.reviews.build(review_params)
+    @review.user_id = current_user.id
 
-  	if @review.save
-  		redirect_to @products_path, notice: => "Review created successfully"
-  	else
-  		render 'products/show'
-  	end
+    if @review.save
+      redirect_to products_path, notice: 'Review created successfully'
+    else
+      render 'products/show'
+    end
   end
 
   def destroy
-  	 @review = Review.find(params[:id])
-  	 @review.destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
 
   private
-
   def review_params
-  	params.require(:comment).permit(:product_id, :comment)
+    params.require(:review).permit(:comment, :product_id)
   end
 
   def load_product
-  	@product = Product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
   end
-
 end
-
 
 
 
